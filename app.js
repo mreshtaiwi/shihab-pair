0bf28d953c085a68a94fc2372efda967019f17c9
fati-ma <fatima.atiyya@yahoo.com>
2021-04-28 11:38:50 +0300

    fatima made changes

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function studentGrades() {
+
+    let grades = [10, 15, 20];
+    
+    for(let i=0; i<grades.length; i++){
+        console.log(grades[i]);
+    }
+
+}
+
+studentGrades();
